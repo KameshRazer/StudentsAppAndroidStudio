e1aee536e2e006400e12c0c28a67052ad3c9f70b
KameshRazer <63731461+KameshRazer@users.noreply.github.com>
2020-04-27 11:09:22 +0530

    Update home.java

--- app/src/main/java/com/example/dash/home.java
+++ app/src/main/java/com/example/dash/home.java
@@ -1,7 +1,9 @@
 package com.example.dash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -12,6 +14,14 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
 public class home extends AppCompatActivity {
 
     Button btnmenu,circular,timetable,ecamp,so;
@@ -19,7 +29,9 @@ public class home extends AppCompatActivity {
     LinearLayout mainmenu;
     Animation fromtop,frombottom;
     ImageView userpicbig;
-    TextView name,rollno;
+    TextView name,rollno,nameHome,rollNoHome;
+    String rollNo,userName,imageURL;
+    DatabaseReference dbRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +45,17 @@ public class home extends AppCompatActivity {
 
         name = (TextView)findViewById(R.id.name);
         rollno = (TextView)findViewById(R.id.rollno);
+        nameHome = (TextView)findViewById(R.id.userhome);
+        rollNoHome = (TextView)findViewById(R.id.rollnohome);
 
         userpicbig = (ImageView)findViewById(R.id.userpicbig);
+
+        Intent i = getIntent();
+        userName =  i.getStringExtra("name_key");
+        rollNo = i.getStringExtra("rollNo_key");
+        imageURL = i.getStringExtra("imageUrl_key");
+
+//        dbRef = FirebaseDatabase.getInstance().getReference().child(rollNo);
 
         fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
         frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
@@ -42,9 +63,18 @@ public class home extends AppCompatActivity {
         maincontent = (RelativeLayout) findViewById(R.id.maincontent);
         mainmenu = (LinearLayout) findViewById(R.id.mainmenu);
 
+        nameHome.setText(userName);
+        rollNoHome.setText(rollNo);
+        System.out.println("Roll No :"+rollNo);
+        Picasso.with(home.this).load(imageURL)
+                .resize(400,350)
+                .into(userpicbig);
+
         btnmenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                name.setText(userName);
+                rollno.setText(rollNo);
                 maincontent.animate().translationX(0);
                 mainmenu.animate().translationX(0);
 
@@ -66,5 +96,6 @@ public class home extends AppCompatActivity {
                 mainmenu.animate().translationX(-735);
             }
         });
+
     }
 }
